5b90c74d4f1134d5e01f1f04a7cd106774d1c3fa
Deveshwaar <deveshwaar99@gmail.com>
2024-03-18 18:12:25 +0530

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Box from '@/components/shared/Box'
+import React from 'react'
+
+function Error() {
+  return (
+    <Box className="flex h-full items-center justify-center">
+      <div className="text-neutral-400">Somthing went wrong.</div>
+    </Box>
+  )
+}
+
+export default Error
