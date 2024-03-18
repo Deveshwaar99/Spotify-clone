bd2dda88cff95eb51ce0557328e4aa733033cf6f
Deveshwaar <deveshwaar99@gmail.com>
2024-03-18 17:56:56 +0530

    refactor:use supabase outside the provider

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,10 +1,11 @@
 'use client'
-import { useClient } from '@/providers/SupabaseProvider'
+
+import { createClient } from '@/utils/supabase/client'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 function SignUpPage() {
-  const supabase = useClient()
+  const supabase = createClient()
   return (
     <div>
       <main className="my-1 px-4 py-2 text-center text-4xl font-bold text-white sm:px-6 lg:px-8">
