eb86785deba34f65874e8e04071f42e83f9101e4
Deveshwaar <deveshwaar99@gmail.com>
2024-03-21 22:20:36 +0530

    fix:sign up page render

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,17 +1,19 @@
 'use client'
+import React, { useEffect } from 'react'
 
-import { getURL } from '@/lib/helpers'
-import { createClient } from '@/utils/supabase/client'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
-function SignUpPage() {
+// import { SignUser } from '@/components/shared/SignUser'
+import { createClient } from '@/utils/supabase/client'
+import { getURL } from '@/lib/helpers'
+import { useRouter } from 'next/navigation'
+
+function Login() {
   const supabaseClient = createClient()
-  const router = useRouter()
   const redirectUrl = getURL()
 
+  const router = useRouter()
   useEffect(() => {
     const { data: authListener } = supabaseClient.auth.onAuthStateChange((event, session) => {
       if (event === 'SIGNED_IN') {
@@ -24,34 +26,36 @@ function SignUpPage() {
     }
   }, [redirectUrl, supabaseClient.auth, router])
   return (
-    <div>
-      <main className="my-1 px-4 py-2 text-center text-4xl font-bold text-white sm:px-6 lg:px-8">
-        <div className="flex flex-col text-left">
-          <p>Signup to start</p>
-          <p>Listening</p>
-        </div>
-      </main>
-      <div className="">
-        <Auth
-          supabaseClient={supabaseClient}
-          providers={['github']}
-          magicLink={true}
-          appearance={{
-            theme: ThemeSupa,
-            variables: {
-              default: {
-                colors: {
-                  brand: '#404040',
-                  brandAccent: '#22c55e',
-                },
+    <div className="">
+      <h2 className="text-center text-3xl">Welcome back!</h2>
+      <Auth
+        supabaseClient={supabaseClient}
+        providers={['github']}
+        magicLink={true}
+        localization={{
+          variables: {
+            sign_up: {
+              confirmation_text: 'Click in the link in your email and login again',
+            },
+          },
+        }}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: '#404040',
+                brandAccent: '#22c55e',
               },
             },
-          }}
-          theme="dark"
-        />
-      </div>
+          },
+        }}
+        theme="dark"
+      />
+      {/* <main className="my-1 text-center text-4xl font-bold text-white">Welcome Back</main> */}
+      {/* <SignUser type="login" /> */}
     </div>
   )
 }
 
-export default SignUpPage
+export default Login
