3d58b03656f6da6850a63189f49b46493f5791f8
Deveshwaar <deveshwaar99@gmail.com>
2024-03-22 13:26:53 +0530

    feat(playbutton): add size class for consistency
    This commit adds the 'size-14' class to the PlayButton

--- src/components/shared/PlayButton.tsx
+++ src/components/shared/PlayButton.tsx
@@ -3,7 +3,7 @@ import { Play } from 'lucide-react'
 const PlayButton = () => {
   return (
     <div>
-      <button className="flex translate-y-1/4 items-center rounded-full bg-green-500 p-4 opacity-0 drop-shadow-md transition hover:scale-110 group-hover:translate-y-0 group-hover:opacity-100">
+      <button className="flex size-14 translate-y-1/4 items-center rounded-full bg-green-500 p-4 opacity-0 drop-shadow-md transition hover:scale-110 group-hover:translate-y-0 group-hover:opacity-100">
         <Play color="black" fill="black" />
       </button>
     </div>
