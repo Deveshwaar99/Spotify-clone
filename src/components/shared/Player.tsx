3f0e1d6a85c9f9db75acfeed28877a0b47b9ddf0
Deveshwaar <deveshwaar99@gmail.com>
2024-03-22 13:28:48 +0530

    refactor(player): improve loading handling and rendering logic

--- src/components/shared/Player.tsx
+++ src/components/shared/Player.tsx
@@ -4,17 +4,33 @@ import useGetSongById from '@/hooks/useGetSongById'
 import useLoadSongUrl from '@/hooks/useLoadSongUrl'
 import usePlayer from '@/hooks/usePlayer'
 import PlayerContent from './PlayerContent'
+import { useMemo } from 'react'
+import { BounceLoader, RiseLoader, ScaleLoader } from 'react-spinners'
 
 function Player() {
   const player = usePlayer()
-  const { song } = useGetSongById(player.activeId)
-  const { songUrl } = useLoadSongUrl(song!)
+  const { song, isLoading: isSongLoading } = useGetSongById(player.activeId)
+  const { songUrl, isLoading: isSongUrlLoading } = useLoadSongUrl(song!)
+
+  const isLoading = isSongLoading || isSongUrlLoading
+  const shouldRender = useMemo(() => {
+    return song && songUrl && player.activeId
+  }, [song, songUrl, player.activeId])
+
+  if (isLoading)
+    return (
+      <div className="flex h-[80px] items-center justify-center bg-neutral-900">
+        <ScaleLoader color="#22c55e" />
+      </div>
+    )
+
+  if (!shouldRender) return null
 
   if (!song || !songUrl || !player.activeId) return null
 
   return (
     <div className="fixed bottom-0 h-[80px] w-full bg-black px-4 py-2">
-      <PlayerContent song={song} songUrl={songUrl} />
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   )
 }
