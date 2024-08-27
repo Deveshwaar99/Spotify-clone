5e468a9bf811224fbdee3560e621f3d32d04356b
Deveshwaar <deveshwaar99@gmail.com>
2024-08-28 00:55:40 +0530

    refactor: replace div with button element for SongItem component
    - Improved accessibility by replacing the outer div with a semantic button element
    - Removed unnecessary React fragment and cleaned up the code structure
    - Ensured consistent use of TypeScript with explicit type imports

--- src/app/(root)/_components/SongItem.tsx
+++ src/app/(root)/_components/SongItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import { Song } from '@/types/types'
+import type { Song } from '@/types/types'
 import PlayButton from '@/components/shared/PlayButton'
 import { useLoadImageUrl } from '@/hooks/useLoadImageUrl'
 
@@ -9,31 +9,29 @@ type SongItemProps = {
   onClick: (id: string) => void
 }
 function SongItem({ data, onClick }: SongItemProps) {
-  let { imageUrl: imagePath } = useLoadImageUrl(data)
+  const { imageUrl: imagePath } = useLoadImageUrl(data)
   return (
-    <>
-      <div
-        onClick={() => onClick(data.id)}
-        className="group relative flex cursor-pointer flex-col items-center justify-center gap-x-4 overflow-hidden rounded-md bg-neutral-400/5 p-3 transition hover:bg-neutral-400/10"
-      >
-        <div className="relative aspect-square h-full w-full overflow-hidden rounded-md">
-          <Image
-            className="object-scale-down"
-            src={imagePath || '/images/placeholder.png'}
-            fill
-            alt="Image"
-          />
-        </div>
-        <div className="flex w-full flex-col items-start gap-y-1 pt-4">
-          <p className="w-full truncate font-semibold">{data.title}</p>
-          <p className="w-full truncate pb-4 text-sm text-neutral-400">By {data.author}</p>
-        </div>
-        <div className="absolute bottom-24 right-5">
-          <PlayButton />
-        </div>
+    <button
+      type="button"
+      onClick={() => onClick(data.id)}
+      className="group relative flex cursor-pointer flex-col items-center justify-center gap-x-4 overflow-hidden rounded-md bg-neutral-400/5 p-3 transition hover:bg-neutral-400/10"
+    >
+      <div className="relative aspect-square h-full w-full overflow-hidden rounded-md">
+        <Image
+          className="object-scale-down"
+          src={imagePath || '/images/placeholder.png'}
+          fill
+          alt={`${data.title} image}`}
+        />
       </div>
-      {}
-    </>
+      <div className="flex w-full flex-col items-start gap-y-1 pt-4">
+        <p className="w-full truncate font-semibold">{data.title}</p>
+        <p className="w-full truncate pb-4 text-sm text-neutral-400">By {data.author}</p>
+      </div>
+      <div className="absolute bottom-24 right-5">
+        <PlayButton />
+      </div>
+    </button>
   )
 }
 
