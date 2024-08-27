01986c06c0344a01fbe0768c84060e00ee769f93
Deveshwaar <deveshwaar99@gmail.com>
2024-08-28 00:54:38 +0530

    refactor: remove unnecessary fragment from PageContent component
    - Simplified the JSX structure by removing an unnecessary React fragment
    - Improved code readability by directly returning the conditional grid layout

--- src/app/(root)/_components/PageContent.tsx
+++ src/app/(root)/_components/PageContent.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Song } from '@/types/types'
+import type { Song } from '@/types/types'
 import SongItem from './SongItem'
 import useOnPlay from '@/hooks/useOnPlay'
 
@@ -14,19 +14,17 @@ function PageContent({ songs }: PageContentProps) {
       {songs.length === 0 ? (
         <div className="mt-4 text-neutral-400">No songs available.</div>
       ) : (
-        <>
-          <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8">
-            {songs.map(item => (
-              <SongItem
-                key={item.id}
-                onClick={id => {
-                  onPlay(id)
-                }}
-                data={item}
-              />
-            ))}
-          </div>
-        </>
+        <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8">
+          {songs.map(item => (
+            <SongItem
+              key={item.id}
+              onClick={id => {
+                onPlay(id)
+              }}
+              data={item}
+            />
+          ))}
+        </div>
       )}
     </>
   )
