86e1a0e1302affc6d0da5690ecaee627347eddd6
Deveshwaar <deveshwaar99@gmail.com>
2024-08-28 14:06:52 +0530

    refactor(ui): rename Error component for clarity

--- src/app/(root)/liked/error.tsx
+++ src/app/(root)/liked/error.tsx
@@ -2,7 +2,7 @@
 
 import Box from '@/components/shared/Box'
 
-const Error = () => {
+const ErrorComponent = () => {
   return (
     <Box className="flex h-full items-center justify-center">
       <div className="text-neutral-400">Something went wrong.</div>
