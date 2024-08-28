cf2c4e93f2e38f1a9f366bcab032ee8f85d4d82a
Deveshwaar <deveshwaar99@gmail.com>
2024-08-28 18:21:28 +0530

    refactor: extract volume and progress bar logic into custom hooks  -remove unused imports

--- src/components/shared/PlayerContent.tsx
+++ src/components/shared/PlayerContent.tsx
@@ -1,25 +1,25 @@
 'use client'
-import { Song } from '@/types/types'
-import React, { useEffect, useMemo, useState } from 'react'
-import MediaItem from './MediaItem'
-import LikeButton from './LikeButton'
-import { Pause, Play, SkipBack, SkipForward, Volume1, Volume2, VolumeX } from 'lucide-react'
-import { Slider } from '../ui/slider'
 import usePlayer from '@/hooks/usePlayer'
-import useSound from 'use-sound'
+import useProgressBar from '@/hooks/useProgressBar'
+import useVolume from '@/hooks/useVolume'
 import { formatDuration } from '@/lib/helpers'
+import type { Song } from '@/types/types'
+import { Pause, Play, SkipBack, SkipForward } from 'lucide-react'
+import { useEffect, useMemo, useState } from 'react'
+import useSound from 'use-sound'
+import { Slider } from '../ui/slider'
+import LikeButton from './LikeButton'
+import MediaItem from './MediaItem'
 import ProgressBar from './ProgressBar'
 type PlayerContentProps = {
   song: Song
   songUrl: string
 }
 function PlayerContent({ song, songUrl }: PlayerContentProps) {
-  const [volume, setVolume] = useState(0.4)
-  const [isPlaying, setIsplaying] = useState(false)
-  const [currentDuration, setCurrentDuration] = useState(0)
-
+  const { volume, toggleVolume, handleVolumeChange, VolumeIcon } = useVolume(0.4)
+  const [isPlaying, setIsplaying] = useState(true)
   const player = usePlayer()
-  //creating the sound object and recieving player controllers
+
   const [play, { pause, sound, duration }] = useSound(songUrl, {
     format: ['mp3'],
     volume: volume,
@@ -31,30 +31,21 @@ function PlayerContent({ song, songUrl }: PlayerContentProps) {
     onpause: () => setIsplaying(false),
   })
 
+  const { currentDuration, setCurrentDuration } = useProgressBar(sound)
+
   const formattedDuration = useMemo(() => formatDuration((duration || 0) / 1000), [duration])
   const formattedCurrentDuration = formatDuration(currentDuration)
-  console.log(formattedCurrentDuration)
 
   const Icon = isPlaying ? Pause : Play
 
-  //volume controls
-  let VolumeIcon
+  useEffect(() => {
+    sound?.play()
+    return () => {
+      sound?.unload()
+    }
+  }, [sound])
 
-  if (volume === 0) {
-    VolumeIcon = VolumeX
-  } else if (volume <= 0.6) {
-    VolumeIcon = Volume1
-  } else VolumeIcon = Volume2
-
-  const handleVolumeChange = (newValue: number[]) => {
-    setVolume(newValue[0])
-  }
-  const toggleVolume = () => {
-    if (volume === 0) return setVolume(0.5)
-    setVolume(0)
-  }
-
-  //Play controls
+  // Play controls
   const handlePlay = () => {
     if (!isPlaying) {
       play()
@@ -73,6 +64,7 @@ function PlayerContent({ song, songUrl }: PlayerContentProps) {
     }
     player.setId(player.ids[nextSongIndex])
   }
+
   const onPlayPrevious = () => {
     if (player.ids.length === 0 || !player.activeId) return
 
@@ -84,27 +76,11 @@ function PlayerContent({ song, songUrl }: PlayerContentProps) {
     player.setId(player.ids[nextSongIndex])
   }
 
-  //Progress controls
-
+  // Progress controls
   const onProgressChange = (newTime: number) => {
     sound?.seek(newTime)
     setCurrentDuration(newTime)
   }
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (sound) {
-        setCurrentDuration(sound.seek() || 0)
-      }
-    }, 1000)
-    return () => clearInterval(interval)
-  }, [sound])
-
-  useEffect(() => {
-    sound?.play()
-    return () => {
-      sound?.unload()
-    }
-  }, [sound])
 
   return (
     <div className="grid h-full grid-cols-2 md:grid-cols-3">
@@ -129,8 +105,9 @@ function PlayerContent({ song, songUrl }: PlayerContentProps) {
             size={25}
             className="cursor-pointer text-neutral-400 transition hover:text-white"
           />
+
           <div
-            onClick={() => {}}
+            // onClick={() => {}}
             className="flex h-10 w-10 items-center justify-center rounded-full bg-white"
           >
             <Icon fill="black" size={25} onClick={() => handlePlay()} />
